5350bdfe4bbcf8fd4cb505514977233448a7b7d4
Mylena Ribeiro <ribeiro.mylena1@gmail.com>
2025-07-13 20:44:43 -0300

    Composição para criar a classe arqueiro mago

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import { Mago } from "./modules/mago.js";
-// import { Personagem } from "./modules/personagem.js";
 import { PersonagemView } from "./components/personagem-view.js";
 import { Arqueiro } from "./modules/arqueiro.js";
+import { ArqueiroMago } from "./modules/arqueiro-mago.js";
 
-// const personagemPedrinho = new Personagem('Pedrinho', 5, 'Mago');
-// const personagemJose = new Personagem('José', 3, 'Arqueiro');
 const magoPedrinho = new Mago('Antonio', 4, 'Fogo', 4, 3)
 const magaJulia = new Mago('Julia', 8, 'Gelo', 7, 10);
 const arqueiroLegolas = new Arqueiro('Legolas', 7, 8);
-const personagens = [magoPedrinho, magaJulia, arqueiroLegolas];
+const arqueiroMagoClint = new ArqueiroMago('Clint', 7, 10, 'Ar', 4, 8);
 
-// const personagens = [personagemPedrinho, personagemJose];
+const personagens = [magoPedrinho, magaJulia, arqueiroLegolas, arqueiroMagoClint];
 
 new PersonagemView(personagens).render();
 
